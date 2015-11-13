8d93b87fe8f79c5d42d046616db4bd03adaad1da
Erik Evenson <erik.e.evenson@gmail.com>
2015-11-13 09:19:59 -0600

    adds a gulp campaign mutateAdd example

--- gulp/campaignService.js
+++ gulp/campaignService.js
@@ -3,7 +3,8 @@ var
   async = require('async'),
   gulp = require('gulp'),
   pd = require('pretty-data').pd,
-  soap = require('soap');
+  soap = require('soap'),
+  uuid = require('uuid');
 
 gulp.task(
   'adWords:campaignService:get',
@@ -35,5 +36,51 @@ gulp.task(
       else console.log(JSON.stringify(results, null, 2));
       cb(err);
     });
+  }
+);
+
+gulp.task(
+  'adWords:campaignService:mutateAdd',
+  'adds Google AdWords campaign',
+  function(cb) {
+    var argv = require('yargs')
+      .boolean('validateOnly')
+      .default(
+        'clientCustomerId',
+        process.env.ADWORDS_CLIENT_CUSTOMER_ID,
+        'clientCustomerId of account'
+      )
+      .default('validateOnly', false, 'validate only')
+      .demand('name', 'name of the budget')
+      .demand('budgetId', 'budgetId to use')
+      .argv;
+
+    var AdWords = require('..');
+
+    var service = new AdWords.CampaignService({
+      validateOnly: argv.validateOnly
+    });
+
+    // This will only work on my account...
+    var operand = new service.Model({
+      name: argv.name,
+      budget: {budgetId: argv.budgetId},
+      advertisingChannelType: 'SEARCH',
+
+      biddingStrategyConfiguration: {
+        biddingStrategyName: 'CD-BIDDING_STRATEGY-' + uuid.v4(),
+        biddingStrategyType: 'MANUAL_CPC'
+      }
+    });
+
+    service.mutateAdd(
+      argv.clientCustomerId,
+      operand,
+      function(err, results) {
+        if (err) console.log(err);
+        else console.log(JSON.stringify(results, null, 2));
+        cb(err);
+      }
+    );
   }
 );
