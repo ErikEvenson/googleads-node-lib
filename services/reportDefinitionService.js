d42296a906c98a4e3e2b902a51c528e564635580
Erik Evenson <erik.e.evenson@gmail.com>
2015-11-24 15:18:14 -0600

    adds reports

--- services/reportDefinitionService.js
+++ services/reportDefinitionService.js
@@ -1,16 +1,43 @@
 var
   _ = require('lodash'),
   async = require('async'),
+  request = require('request'),
   soap = require('soap');
 
 var AdWordsService = require('./adWordsService');
 var types = require('../types/reportDefinitionField');
+var reportUrl = 'https://adwords.google.com/api/adwords/reportdownload/v201509';
+
 
 function Service(options) {
   var self = this;
   AdWordsService.call(self, options);
   self.Collection = types.collection;
   self.Model = types.model;
+
+  self.getReport = function(options, done) {
+    async.series([
+      // get credentials
+      self.refresh,
+      // get report
+      function(cb) {
+        var opts = {
+          body: '__rdxml=' + encodeURIComponent(options.rdxml),
+          headers: {
+            Authorization: 'Bearer ' + self.credentials.access_token,
+            developerToken: self.options.ADWORDS_DEVELOPER_TOKEN,
+            clientCustomerId: options.clientCustomerId,
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          method: 'POST',
+          url: reportUrl,
+        };
+
+        request(opts, done);
+      }
+    ],
+    done);
+  };
 
   self.getReportFields = function(options, done) {
     async.waterfall([
