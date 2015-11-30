1e9632a2ee405bb382bbc593d9bf3bc9757c9fb5
Erik Evenson <erik.e.evenson@gmail.com>
2015-11-30 15:42:28 -0600

    adds a simple fields

--- gulp/campaignPerformanceReport.js
+++ gulp/campaignPerformanceReport.js
@@ -6,6 +6,62 @@ var
   pd = require('pretty-data').pd;
 
 gulp.task(
+  'adWords:campaignPerformanceReport:total',
+  'Google AdWords reports',
+  function(cb) {
+    var argv = require('yargs')
+      .default(
+        'clientCustomerId',
+        process.env.ADWORDS_CLIENT_CUSTOMER_ID,
+        'clientCustomerId of account'
+      )
+      .argv;
+
+    var AdWords = require('..');
+    var report = new AdWords.CampaignPerformanceReport();
+
+    var fieldNames = [
+      // 'Period',
+      // 'AccountDescriptiveName',
+      // 'Amount',
+      'CampaignId',
+      'CampaignName',
+      // 'CampaignStatus',
+      // 'Cost',
+      // 'CustomerDescriptiveName',
+      // 'Date',
+      // 'EndDate',
+      // 'Impressions',
+      // 'Interactions',
+      // 'Month',
+      // 'MonthOfYear',
+      // 'PrimaryCompanyName',
+      // 'Quarter',
+      // 'StartDate',
+      'TotalCost',
+      'Week'
+    ];
+
+    var options = {
+      clientCustomerId: argv.clientCustomerId,
+      fieldNames: fieldNames
+    };
+
+    report.getReport(options, function(err, incoming, response) {
+      if (err) return cb(err);
+      console.log(pd.xml(response));
+
+      // parseString(response, function (err, result) {
+      //   console.log(result.report.table[0].row[0]['$']);
+      //   cb();
+      // });
+
+      cb();
+    });
+  }
+);
+
+gulp.task(
   'adWords:campaignPerformanceReport',
   'Google AdWords reports',
   function(cb) {
