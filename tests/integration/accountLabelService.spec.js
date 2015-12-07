f49fbe21d43a5e13218df8809170a5acceaeba12
Erik Evenson <erik.e.evenson@gmail.com>
2015-12-07 13:57:41 -0600

    adds more integration tests

--- tests/integration/accountLabelService.spec.js
+++ tests/integration/accountLabelService.spec.js
@@ -1,4 +1,7 @@
-var expect = require('expect');
+var
+  async = require('async'),
+  expect = require('expect'),
+  uuid = require('uuid');
 
 describe('AccountLabelService', function() {
   var AdWords = require('../..');
@@ -25,6 +28,63 @@ describe('AccountLabelService', function() {
         expect(err).toNotExist();
         expect(results.labels).toExist();
         return done(err);
+      }
+    );
+  });
+
+  it('should create, set, and remove an account label', function(done) {
+    var label = null;
+
+    async.series(
+      [
+        // add the account label
+        function(cb) {
+          service.addAccountLabel(
+            process.env.ADWORDS_CLIENT_CUSTOMER_ID,
+            uuid.v4(),
+            function(err, results) {
+              expect(err).toNotExist();
+              expect(results.labels).toExist();
+              expect(results.labels.length).toEqual(1);
+              label = results.labels.pop();
+              return cb(err, label);
+            }
+          );
+        },
+        // set account label
+        function(cb) {
+          var labelName = 'TESTNAME';
+
+          service.setAccountLabel(
+            process.env.ADWORDS_CLIENT_CUSTOMER_ID,
+            label.get('id'),
+            labelName,
+            function(err, results) {
+              expect(err).toNotExist();
+              expect(results.labels).toExist();
+              expect(results.labels.length).toEqual(1);
+              label = results.labels.pop();
+              expect(label.get('name')).toEqual(labelName);
+              return cb(err, label);
+            }
+          );
+        },
+        // remove account label
+        function(cb) {
+          service.removeAccountLabel(
+            process.env.ADWORDS_CLIENT_CUSTOMER_ID,
+            label.get('id'),
+            function(err, results) {
+              expect(err).toNotExist();
+              expect(results.labels).toExist();
+              expect(results.labels.length).toEqual(1);
+              return cb(err);
+            }
+          );
+        }
+      ],
+      function(err, results) {
+        done(err);
       }
     );
   });
